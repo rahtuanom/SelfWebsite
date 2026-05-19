bca0b84503f3f532a501cbd1205c68cb0b8c60a1
Rahtu Anom <rahtuanom10@gmail.com>
2026-05-19 23:37:05 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   // Aktifkan export statis (wajib untuk GitHub Pages)
   output: "export",
   
-  // Base path menyesuaikan dengan nama repository kamu di GitHub
-  basePath: "/SelfWebsite",
-  
   // Menonaktifkan optimasi image default karena export statis tidak mendukungnya
   images: {
     unoptimized: true,

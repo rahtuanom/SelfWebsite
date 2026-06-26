b4eb1cd7dded30ed550d50d347f29b49ba446f88
Rahtu Anom <rahtuanom10@gmail.com>
2026-06-26 12:55:11 +0800

    fixing bug switch halaman

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   // Aktifkan export statis (wajib untuk GitHub Pages)
   output: "export",
   
+  // Memastikan folder statis dibentuk dengan index.html (misal: /about/index.html)
+  trailingSlash: true,
+  
   // Menonaktifkan optimasi image default karena export statis tidak mendukungnya
   images: {
     unoptimized: true,
